6c5ecf6719663f2ebfe46ac0e4306660e3dfd240
JarPr <131611487+JarPr@users.noreply.github.com>
2023-11-19 16:57:47 +0100

    react-hw-03-image-finder

---
+++ src/components/ErrorText/ErrorText.jsx
@@ -0,0 +1,5 @@
+import { Error } from './ErrorText.styled';
+
+export const ErrorText = () => {
+  return <Error>Something went wrong! Please reload this page! 🥹</Error>;
+};
---
+++ src/components/api.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const ApiKey = '39872924-85aaaf490e1b4ae0c12b933b4';
+
+axios.defaults.baseURL = `https://pixabay.com/api/`;
+
+export const fetchImages = async (query, page) => {
+  const response = await axios.get(
+    `?q=${query}&page=${page}&key=${ApiKey}&image_type=photo&orientation=horizontal&per_page=12`
+  );
+  return response.data.hits;
+};
